3c115d2fd90f5c5c39f28b3fd50e92685342c600
Dan Suceava <dans@axosoft.com>
2018-04-02 11:35:41 -0700

    track send stats

--- src/public_api/index.js
+++ src/public_api/index.js
@@ -13,11 +13,17 @@ const gkConfig = {
   useRequestLogger: false
 };
 
-const setupFn = (app, gkKafka, mongoose) => {
-  console.log('setup');
+const stats = {
+  message_count: 0,
+  time_average: 0,
+  time_total: 0
+}
 
+const setupFn = (app, gkKafka, mongoose) => {
   app.post('/api/kafka', (req, res) => {
     // send a kafka message
+    const dt = new Date();
+
     const topic = req.query.topic || 'kafka1';
     const event = req.query.event || 'kafka1-event1';
     const size = Math.min(parseInt(req.query.size) || 100, 10000);
@@ -40,14 +46,27 @@ const setupFn = (app, gkKafka, mongoose) => {
         message: msg
       }
     );
+
+    const duration = new Date() - dt;
+    stats.time_total += duration;
+    stats.message_count++;
+    stats.time_average = stats.time_total / stats.message_count;
+
+    console.log(`sent message in ${duration}`);
   });
 
   app.get('/api/kafka', (req, res) => {
     // get kafka stats
+    res.status(200)
+      .set('Content-Type', 'application/json')
+      .send(stats);
   });
 };
 
 Api(APP_NAME, 5432, setupFn, gkConfig)
   .then(() => {
     console.log('api ready');
+  })
+  .catch((e) => {
+    console.error(e);
   });
