ab95dc2bac96da7f147b00ae6946b7f185eef137
Dan Suceava <dans@axosoft.com>
2018-04-04 16:24:15 -0700

    add an endpoint to forever send a message

--- src/public_api/index.js
+++ src/public_api/index.js
@@ -19,46 +19,65 @@ const stats = {
   size_total: 0,
   time_average: 0,
   time_total: 0
+};
+
+const sendMessage = (topic, event, size) => {
+  const dt = new Date();
+
+  console.log(`sending '${size}' byte message for '${event}' envent on '${topic}' topic`);
+
+  let i=0, j=size;
+  const chars = [];
+  while (i < j) {
+    chars[i++] = chars[j--] = String.fromCharCode(65 + 24 * Math.random());
+  }
+  const msg = chars.join('');
+  console.log('msg', msg);
+
+  gkKafka.sendMessage(
+    APP_NAME,
+    topic,
+    event,
+    {
+      message: msg
+    }
+  );
+
+  stats.message_count++;
+
+  const duration = new Date() - dt;
+  stats.time_total += duration;
+  stats.time_average = stats.time_total / stats.message_count;
+  stats.size_total += size;
+  stats.size_average = stats.size_total / stats.message_count;
+
+  console.log(`sent message in ${duration}`);
+};
+
+const foreverSend = (topic, event, size, delay) => {
+  sendMessage(topic, event, size);
+  setTimeout(() => foreverSend(topic, event, size, delay), delay);
 }
 
 const setupFn = (app, gkKafka, mongoose) => {
   app.post('/api/kafka', (req, res) => {
     // send a kafka message
-    const dt = new Date();
-
     const topic = req.query.topic || 'kafka1';
     const event = req.query.event || `${topic}-event1`;
     const size = Math.min(parseInt(req.query.size) || 100, 10000);
 
-    console.log(`sending '${size}' byte message for '${event}' envent on '${topic}' topic`);
+    sendMessage(topic, event, size);
 
-    let i=0, j=size;
-    const chars = [];
-    while (i < j) {
-      chars[i++] = chars[j--] = String.fromCharCode(65 + 24 * Math.random());
-    }
-    const msg = chars.join('');
-    console.log('msg', msg);
-
-    gkKafka.sendMessage(
-      APP_NAME,
-      topic,
-      event,
-      {
-        message: msg
-      }
-    );
-
-    stats.message_count++;
-
-    const duration = new Date() - dt;
-    stats.time_total += duration;
-    stats.time_average = stats.time_total / stats.message_count;
-    stats.size_total += size;
-    stats.size_average = stats.size_total / stats.message_count;
-
-    console.log(`sent message in ${duration}`);
     res.sendStatus(200);
+  });
+
+  app.post('/api/kafka/forever', (req, res) => {
+    const topic = req.query.topic || 'kafka1';
+    const event = req.query.event || `${topic}-event1`;
+    const size = Math.min(parseInt(req.query.size) || 100, 10000);
+    const delay = Math.min(parseInt(req.query.delay) || 10, 60000);
+
+    foreverSend(topic, event, size, delay);
   });
 
   app.get('/api/kafka', (req, res) => {
